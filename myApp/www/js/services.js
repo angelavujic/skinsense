4255107ddebf58509b1681184248b67aa9343cb9
Stephen Song <stephen.song@gatech.edu>
2015-11-08 02:29:44 -0500

    Finished navigations and layouts for results screens

--- myApp/www/js/services.js
+++ myApp/www/js/services.js
@@ -1,4 +1,36 @@
 angular.module('starter.services', [])
+.factory('States', function() {
+  var states = [{
+    id: 1,
+    color: '#fabbbb',
+    icon: '',
+    header: 'No Melanoma',
+    body: 'The photographed skin appeared to be a benign lesion. While no medical action is needed, continue to care for your skin and repeat this test weekly.'
+  }, {
+    id: 2,
+    color: '#fabbbb',
+    icon: '',
+    header: 'Suspected Melanoma',
+    body: 'There is a chance that your skin is developing melanoma. Contact your doctor or dermatologist to remove the suspicious lesion and examine your skin.'
+  }, {
+    id: 3,
+    color: '#fabbbb',
+    icon: '',
+    header: 'Melanoma Confirmed',
+    body: 'Contact your doctor or dermatologist to remove the suspicious lesion and examine your skin.'
+  }];
+
+  return {
+    get: function(stateId) {
+      for (var i = 0; i < states.length; i++) {
+        if (states[i].id === parseInt(stateId)) {
+          return states[i];
+        }
+      }
+      return null;
+    }
+  }
+})
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
